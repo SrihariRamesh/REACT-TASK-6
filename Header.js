5cb56723f678661bd16c42dcf3c376588d88ca69
SrihariRamesh <142098192+SrihariRamesh@users.noreply.github.com>
2023-11-10 22:44:36 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Srihari Ramesh</h1>
+      <div className="tile">
+        <h3>Front End Developer</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
